4fb962304a2aba864bd85431fd0b52cdc34c2505
ahmadraza365 <ahmadrazabhatti30@gmail.com>
2023-05-30 22:24:10 +0500

    Fixing Google analytics script

--- components/analytics/GoogleAnalytics.js
+++ components/analytics/GoogleAnalytics.js
@@ -6,11 +6,11 @@ const GAScript = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
       />
 
-      <Script strategy="lazyOnload" id="ga-script">
+      <Script strategy="afterInteractive" id="ga-script">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
